4c4f9612bb1463f68b79bd1db45d166991246c74
Xi-Plus <huangxuanyuxiplus@gmail.com>
2015-10-11 14:29:23 +0800

    global config

---
+++ global_config_example.php
@@ -0,0 +1,7 @@
+<?php
+$config['sql_path']=__DIR__.'/sql/sql.php';
+$config['facebook_sdk_path']=__DIR__.'/facebook-php-sdk-v4-5.0-dev/src/Facebook/autoload.php';
+$config['app_id']='app_id';
+$config['app_secret']='app_secret';
+$config['access_token']='access_token';
+?>
