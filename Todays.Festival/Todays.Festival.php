3f1ce0bc3abe5b406e1bb11026c1fddd83c724f5
Xi-Plus <huangxuanyuxiplus@gmail.com>
2015-10-11 14:31:44 +0800

    Todays.Festival

---
+++ Todays.Festival/Todays.Festival.php
@@ -0,0 +1,49 @@
+<?php
+ini_set("display_errors",1);
+if(PHP_SAPI!="cli"){
+	echo "No permission.";
+	exit;
+}
+require_once(__DIR__.'/../global_config.php');
+require_once(__DIR__.'/config.php');
+require_once($config['sql_path']);
+require_once($config['facebook_sdk_path']);
+
+$fb = new Facebook\Facebook([
+	'app_id' => $config['app_id'],
+	'app_secret' => $config['app_secret'],
+	'default_access_token' => $config['access_token'],
+	'default_graph_version' => 'v2.5',
+]);
+$response = $fb->get('/me/accounts')->getDecodedBody();
+foreach($response["data"] as $temp){
+	if($temp["id"]==$config['page_id']){
+		$page_token=$temp["access_token"];
+		break;
+	}
+}
+
+$message=date("m")."月".date("d")."日的節假日和習俗有\n";
+
+$html=file_get_contents("https://zh.wikipedia.org/zh-tw/".date("m")."月".date("d")."日");
+$html=str_replace(array("\t","\n"),"",$html);
+$start=strpos($html,"節假日和習俗");
+$html=substr($html, $start);
+$pattern='/<h2>.*?節假日和習俗.*?<\/h2>.*?<ul>(.*?)<\/ul>/';
+preg_match($pattern, $html ,$match);
+$html=$match[1];
+$pattern='/<li>(.*?)<\/li>/';
+preg_match_all($pattern, $html ,$match);
+foreach($match[1] as $temp){
+	$temp=strip_tags($temp);
+	$temp=preg_replace("/\[.*?\]/", "", $temp);
+	$message.="\n".$temp;
+}
+$message.="\n\nhttps://zh.wikipedia.org/zh-tw/".date("m")."月".date("d")."日#.E8.8A.82.E5.81.87.E6.97.A5.E5.92.8C.E4.B9.A0.E4.BF.97";
+echo $message."\n";
+$params = array(
+	"message"=>$message
+);
+$response=$fb->post("/".$config['page_id']."/feed",$params,$page_token)->getDecodedBody();
+var_dump($response);
+?>
