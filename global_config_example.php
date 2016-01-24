38108f49058fad5c748226280cd8e50e8fee3851
Xi-Plus <huangxuanyuxiplus@gmail.com>
2016-01-24 20:50:43 +0800

    clone all function to func folder

--- global_config_example.php
+++ global_config_example.php
@@ -1,6 +1,6 @@
 <?php
-$config['sql_path']=__DIR__.'/sql/sql.php';
-$config['facebook_sdk_path']=__DIR__.'/facebook-php-sdk-v4-5.0-dev/src/Facebook/autoload.php';
+$config['sql_path']=__DIR__.'/function/SQL-function/sql.php';
+$config['facebook_sdk_path']=__DIR__.'/function/facebook-php-sdk-v4/src/Facebook/autoload.php';
 $config['app_id']='app_id';
 $config['app_secret']='app_secret';
 $config['access_token']='access_token';
