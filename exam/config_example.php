96bca532a4f0e992e787ceccd9d473c220372840
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-25 22:30:44 +0800

    rm exam
    split to https://github.com/Xi-Plus/Test-Countdown

--- exam/config_example.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config['page_id']='800668270014037';
-$config['exam_list']=array(
-	array("text"=>"學測 還有 \D 天 \W 週","year"=>2016,"month"=>1,"date"=>22),
-	array("text"=>"指考 還有 \D 天 \W 週","year"=>2016,"month"=>7,"date"=>1)
-);
-?>
